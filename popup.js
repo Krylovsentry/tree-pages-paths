f5e9dfb4cfc53759dce521065e73e2b1c19fe669
Krylovsentry <krylovsentry@yandex.ru>
2017-05-05 18:01:38 +0300

    wip

--- popup.js
+++ popup.js
@@ -3,8 +3,8 @@
         currentUrlAsArray,
         urlsObject,
         urlsArray,
-        root;
-
+        root,
+        addingPoint;
     //
     // chrome.storage.sync.clear();
     chrome.storage.sync.get("urls", function (urls) {
@@ -16,6 +16,7 @@
         let treeBlock = document.getElementById('tree');
 
         root = createRootDiv(treeBlock);
+        addingPoint = root;
 
         if (urlsObject.urls) {
             urlsArray = urlsObject.urls;
@@ -35,19 +36,32 @@
         });
     };
 
-
+    //GOD, EXCUSE ME FOR THIS
     let throughUrls = function (urlPart, i, arr) {
         let fullUrl = arr.slice(0, i + 1).reduce(function (prev, curr, i) {
             return prev + '/' + curr;
         });
-        let nodeElement = createNode().init().setURLValue(fullUrl, urlPart);
-        if (!i) {
-            root.addChild(nodeElement);
-        } else {
-            root.getChildren[root.getChildren.length - 1].addChild(nodeElement);
+        let nodeElement = createNode();
+        nodeElement.setURLValue(fullUrl, urlPart);
+        let isAdding = true;
+
+        addingPoint.getChildren.forEach(function (child) {
+            if (child.getUrl() === fullUrl) {
+                addingPoint = child;
+                isAdding = false;
+            }
+        });
+
+        if (isAdding) {
+            if (!i) {
+                addingPoint.addChild(nodeElement);
+                addingPoint = root;
+            } else {
+                addingPoint.getChildren[addingPoint.getChildren.length - 1].addChild(nodeElement);
+                addingPoint = root;
+            }
         }
     };
-
 
     function prepareTree() {
         if (urlsArray) {
@@ -65,9 +79,7 @@
         //
     }
 
-//Node for tree
     function createNode() {
-
         let containerElement,
             nodeElement,
             expandElement,
@@ -75,60 +87,42 @@
             url,
             children;
 
-        function node() {
+        containerElement = document.createElement('ul');
+        containerElement.className = 'container-element';
+        nodeElement = document.createElement('li');
+        nodeElement.className = 'node-element';
+        expandElement = document.createElement('div');
+        expandElement.className = 'expand-element';
+        contentElement = document.createElement('div');
+        contentElement.className = 'content-element';
+
+        children = [];
+
+        //register on click for content element
+        contentElement.onclick = function () {
+            chrome.tabs.create({url: currentRootUrl + '/' + url});
+        };
+
+        containerElement.appendChild(nodeElement);
+        nodeElement.appendChild(expandElement);
+        nodeElement.appendChild(contentElement);
+
+        return {
+            getElement: containerElement,
+            addChild: function (child) {
+                containerElement.appendChild(child.getElement);
+                children.push(child);
+            },
+            getUrl: function () {
+                return url;
+            },
+            getChildren: children,
+            setURLValue: function (fullUrl, urlValue) {
+                url = fullUrl;
+                contentElement.innerHTML = urlValue;
+            }
         }
 
-        node.init = function () {
-            containerElement = document.createElement('ul');
-            containerElement.className = 'container-element';
-            nodeElement = document.createElement('li');
-            nodeElement.className = 'node-element';
-            expandElement = document.createElement('div');
-            expandElement.className = 'expand-element';
-            contentElement = document.createElement('div');
-            contentElement.className = 'content-element';
-
-            children = [];
-
-            //register on click for content element
-            contentElement.onclick = function () {
-                chrome.tabs.create({url: currentRootUrl + '/' + url});
-            };
-
-            containerElement.appendChild(nodeElement);
-            nodeElement.appendChild(expandElement);
-            nodeElement.appendChild(contentElement);
-            return this;
-        };
-
-        node.getElement = function () {
-            return containerElement;
-        };
-
-        node.open = function () {
-
-        };
-
-        node.close = function () {
-
-        };
-
-        node.addChild = function (child) {
-            containerElement.appendChild(child.getElement());
-            children.push(child);
-        };
-
-        node.getURL = function () {
-            return url;
-        };
-
-        node.setURLValue = function (fullUrl, urlValue) {
-            url = fullUrl;
-            contentElement.innerHTML = urlValue;
-            return this;
-        };
-
-        return node;
     }
 
 
@@ -143,7 +137,7 @@
             rootElement: rootElement,
             getChildren: children,
             addChild: function (child) {
-                rootElement.appendChild(child.getElement());
+                rootElement.appendChild(child.getElement);
                 children.push(child);
             }
         }
